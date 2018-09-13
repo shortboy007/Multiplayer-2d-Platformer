703a94abab86316de4eb41bd5eb48559a67731cc
Shortboy007 <andrewmorgan865@u.boisestate.edu>
2018-09-13 11:09:27 -0600

    Prototype

---
+++ Assets/_Scripts/BallDropper.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class BallDropper : MonoBehaviour
+{
+    
+    
+    public GameObject BallPrefab;
+    
+    public float speed = 5f;
+    
+    public float leftAndRightEdge = 20f;
+    
+    public float chanceToChangeDirections = 0.01f;
+    
+    public float secondsBetweenBallDrops = 5f;
+
+    void Start()
+    {
+        Invoke("DropBall", 2f);
+    }
+    void DropBall()
+    {
+        GameObject Ball = Instantiate<GameObject>(BallPrefab);
+        Ball.transform.position = transform.position;
+        Invoke("DropBall", secondsBetweenBallDrops);
+    }
+
+    void Update()
+    {
+        Vector3 pos = transform.position;
+        pos.x += speed * Time.deltaTime;
+        transform.position = pos;
+
+        if (pos.x < -leftAndRightEdge)
+        {
+            speed = Mathf.Abs(speed);
+        }
+        else if (pos.x > leftAndRightEdge)
+        {
+            speed = -Mathf.Abs(speed);
+        }
+    }
+        void FixedUpdate()
+        {
+            if (Random.value < chanceToChangeDirections)
+            {
+                speed *= -1;
+            }
+        }
+}
---
+++ Assets/_Scripts/CollisionScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+	void OnCollisionEnter(Collision coll)
+	{
+		GameObject collidedWith = coll.gameObject;
+		if (collidedWith.tag == "Ball")
+		{
+			Destroy(collidedWith);
+}
+	}
+}
